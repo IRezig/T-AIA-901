9b211405fc410f4d7197261d765f0304bcc24b49
ibtissem <rezig.ibtissam@gmail.com>
2020-09-14 16:02:12 +0200

    add Language

--- live-recognizer.py
+++ live-recognizer.py
@@ -12,6 +12,9 @@ with sr.Microphone() as source:
     # read the audio data from the default microphone
     audio_data = r.record(source, duration=duration)
     print("Recognizing...")
+    # Language
+    # text = r.recognize_google(audio_data, language="es-ES")
+    
     # convert speech to text
     text = r.recognize_google(audio_data)
     print(text)
