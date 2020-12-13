352a46832275f091ad63379450488fe1011f104f
ibtissem <rezig.ibtissam@gmail.com>
2020-12-13 19:16:14 +0100

    $1

--- live-recognizer.py
+++ live-recognizer.py
@@ -1,20 +1,14 @@
 import speech_recognition as sr
-import sys
 
-#read duration from the arguments
-duration = int("2")
-
-# initialize the recognizer
+# obtain audio from the microphone
 r = sr.Recognizer()
-print("Please talk")
-
 with sr.Microphone() as source:
-    # read the audio data from the default microphone
-    audio_data = r.record(source, duration=duration)
-    print("Recognizing...")
-    # Language
-    # text = r.recognize_google(audio_data, language="es-ES")
-    
-    # convert speech to text
-    text = r.recognize_google(audio_data)
-    print(text)
+    print("Say something!")
+    audio = r.record(source, duration=2)
+
+try:
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
